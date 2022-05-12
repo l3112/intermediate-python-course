80fe83090b2b47dd3b2a9b52e146d3810f2847c5
Morgan Lucas <lucaslmorgan31@gmail.com>
2022-05-12 18:34:53 -0500

    multidice

--- dice_roller.py
+++ dice_roller.py
@@ -2,7 +2,12 @@ import random
 dice_rolls = 2
 dice_sum = 0
 for i in range(0,dice_rolls):
-  roll = random.randint(1,6)
+  roll = random.randint(1,12)
   dice_sum += roll
-  print(f'You rolled a {roll}')
+  if roll == 1:
+    print(f'You rolled a {roll}! Sucks')
+  elif roll == 12:
+    print(f'You rolled a {roll}! Fire!')
+  else:
+    print(f'You rolled a {roll}')
 print(f'You have rolled a total of {dice_sum}')
